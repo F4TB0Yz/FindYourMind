b7ab118a7dc9092aebfabf4cb914c05835ec6387
F4TB0Yz <jfduarte09@gmail.com>
2025-10-11 18:36:51 -0500

    refactor: Eliminar comentarios redundantes en constantes de animación, colores y cadenas para mejorar la claridad del código

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 /// Constantes de colores utilizadas en toda la aplicación
-/// 
-/// Este archivo centraliza todos los colores personalizados para
-/// mantener consistencia visual y facilitar cambios de tema
 class AppColors {
   // Prevenir instanciación
   AppColors._();
@@ -14,10 +11,4 @@ class AppColors {
   
   /// Color de fondo oscuro alternativo
   static const Color darkBackgroundAlt = Color(0xFF1E1E1E);
-
-  // Agregar aquí más colores según sea necesario
-  // Por ejemplo:
-  // static const Color primary = Color(0xFF...);
-  // static const Color secondary = Color(0xFF...);
-  // static const Color accent = Color(0xFF...);
 }
--- lib/core/constants/string_constants.dart
+++ lib/core/constants/string_constants.dart
@@ -1,22 +1,10 @@
 /// Constantes de texto utilizadas en toda la aplicación
-/// 
-/// Este archivo centraliza todos los strings hardcodeados para
-/// facilitar la localización futura y mantener consistencia
 class AppStrings {
   // Prevenir instanciación
   AppStrings._();
 
   // Títulos de pantallas
-  /// Título de la pantalla de hábitos (en mayúsculas)
   static const String habitsTitle = 'HABITOS';
-  
-  /// Título alternativo de la pantalla de hábitos (capitalizado)
-  static const String habitsLabel = 'Habitos';
-
-  // Títulos de otras pantallas
   static const String notesTitle = 'NOTAS';
   static const String tasksTitle = 'TAREAS';
-
-  // Labels comunes
-  // Agregar aquí más strings según sea necesario
 }
